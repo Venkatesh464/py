8cd4581665937393f1b0ed411e3bf4e8abc8df48
Venkatesh464 <donkavenkatesh6465@gmail.com>
2023-05-11 14:59:01 +0530

    modified sample.py

--- sample.py
+++ sample.py
@@ -1,11 +1,6 @@
 import calendar
 
-yy = 2023  # year
-mm = 5    # month
+yy = 2023
+mm = 5
 
-# To take month and year input from the user
-# yy = int(input("Enter year: "))
-# mm = int(input("Enter month: "))
-
-# display the calendar
 print(calendar.month(yy, mm))
